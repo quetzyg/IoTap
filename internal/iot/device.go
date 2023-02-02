d0b6ef668e5bac2378d6862937bbebec03641186
Quetzy Garcia <quetzyg@altek.org>
2023-02-02 16:35:59 +0000

    feat(IoT): add DeviceFetcher() function

--- internal/iot/device.go
+++ internal/iot/device.go
@@ -1,6 +1,9 @@
 package iot
 
 import (
+	"encoding/json"
+	"io"
+	"log"
 	"net"
 	"net/http"
 )
@@ -15,3 +18,40 @@ type Device interface {
 }
 
 type Devices map[string]Device
+
+const (
+	userAgentHeader = "User-Agent"
+	userAgent       = "IoTune/0.1"
+
+	ContentTypeHeader = "Content-Type"
+	JSONMimeType      = "application/json"
+)
+
+// DeviceFetcher performs an HTTP request to fetch a Device.
+func DeviceFetcher(client *http.Client, r *http.Request, dev Device) (Device, error) {
+	r.Header.Set(userAgentHeader, userAgent)
+
+	response, err := client.Do(r)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(body io.ReadCloser) {
+		err = body.Close()
+		if err != nil {
+			log.Printf("error closing response body: %v", err)
+		}
+	}(response.Body)
+
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(b, dev)
+	if err != nil {
+		return nil, err
+	}
+
+	return dev, nil
+}
