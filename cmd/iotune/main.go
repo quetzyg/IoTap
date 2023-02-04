908b107d354b5bab123ebcc073748a091faad5ce
Quetzy Garcia <quetzyg@altek.org>
2023-02-04 06:16:01 +0000

    feat(main): allow passing the driver to use

--- cmd/iotune/main.go
+++ cmd/iotune/main.go
@@ -2,22 +2,45 @@ package main
 
 import (
 	"errors"
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/Stowify/IoTune/internal/iot"
 	"github.com/Stowify/IoTune/internal/iot/device/shelly"
 	"github.com/Stowify/IoTune/internal/network"
 )
 
+var (
+	driver string
+	prober iot.Prober
+)
+
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Flag declaration
+	flag.StringVar(&driver, "driver", shelly.Driver, "Specify the IoT driver. Default is "+shelly.Driver)
+	flag.Usage = func() {
+		fmt.Println("Usage:")
+		fmt.Printf("%s [-driver <driver name>] <config file>\n", os.Args[0])
+	}
+	flag.Parse()
 }
 
 func main() {
+	switch driver {
+	case shelly.Driver:
+		prober = &shelly.Prober{}
+	default:
+		log.Fatalf("unknown driver: %s", driver)
+	}
+
 	tuner := iot.NewTuner()
 
-	log.Println("Starting IoT device scan...")
-	err := tuner.Scan(network.Address(), shelly.ProbeRequest)
+	log.Printf("Starting IoT device scan using the %s driver...", driver)
+	err := tuner.Scan(network.Address(), prober)
 	log.Println("done!")
 
 	var pe iot.ProbeErrors
@@ -31,5 +54,5 @@ func main() {
 
 	devices := tuner.Devices()
 
-	log.Printf("Known IoT devices found: %d\n", len(devices))
+	log.Printf("IoT devices found: %d\n", len(devices))
 }
