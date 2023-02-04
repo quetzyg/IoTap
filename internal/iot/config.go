5e0aea71cba6b47f6cafb291a57a52772ef55dfd
Quetzy Garcia <quetzyg@altek.org>
2023-02-04 17:22:24 +0000

    feat(Config): add interface + empty configuration error + LoadConfig() function

---
+++ internal/iot/config.go
@@ -0,0 +1,36 @@
+package iot
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+// Config defines the methods an IoT device configuration instance should implement.
+type Config interface {
+	Driver() string
+	MakeRequests(Device) ([]*http.Request, error)
+	Empty() bool
+}
+
+var ErrConfigurationEmpty = errors.New("empty configuration")
+
+// LoadConfig from an I/O reader and unmarshal the data into a Config instance.
+func LoadConfig(reader io.Reader, config Config) error {
+	b, err := io.ReadAll(reader)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(b, &config)
+	if err != nil {
+		return err
+	}
+
+	if config.Empty() {
+		return ErrConfigurationEmpty
+	}
+
+	return nil
+}
