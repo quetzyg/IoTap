d3d5afb4193d1576535cdbbc70273f659ee34c6d
Quetzy Garcia <quetzyg@altek.org>
2023-02-04 06:02:52 +0000

    refactor(Tuner): rearrange the probe logic and simplify it

--- internal/iot/tuner.go
+++ internal/iot/tuner.go
@@ -1,13 +1,11 @@
 package iot
 
 import (
-	"errors"
 	"net"
 	"net/http"
-	"net/url"
 )
 
-// Tuner holds Devices found during a network scan.
+// A Tuner holds Devices found during a network scan.
 // It also has the ability to set their configuration.
 type Tuner struct {
 	devices Devices
@@ -20,40 +18,32 @@ func NewTuner() *Tuner {
 	}
 }
 
-// Probe an IP address for known IoT devices.
-func Probe(client *http.Client, ip net.IP, prs ...ProbeRequest) (Device, error) {
-	for _, pr := range prs {
-		r, dev, err := pr(ip)
-		if err != nil {
-			return nil, err
-		}
-
-		dev, err = DeviceFetcher(client, r, dev)
-		var ue *url.Error
-		if errors.As(err, &ue) {
-			// Ignore timeouts, refused connections and other classic HTTP shenanigans,
-			// since - normally - it will mean that there's not device at that IP.
-			continue
-		}
-
-		// The Device might be nil, so the caller will have to validate it
-		return dev, err
+// Probe an IP address for a specific IoT device.
+func Probe(client *http.Client, ip net.IP, prober Prober) (Device, error) {
+	r, dev, err := prober.MakeRequest(ip)
+	if err != nil {
+		return nil, err
 	}
 
-	return nil, nil
+	dev, err = DeviceFetcher(client, r, dev)
+	if prober.IgnoreError(err) {
+		return dev, nil
+	}
+
+	return dev, err
 }
 
-// ScanResult represents the outcome of an IP address scan operation.
-type ScanResult struct {
+// ProbeResult represents the outcome of an IP address probe operation.
+type ProbeResult struct {
 	dev Device
 	err *ProbeError
 }
 
-// scanIP probes a specific IP and passes the result to a channel.
-func scanIP(ch chan<- *ScanResult, ip net.IP, prs ...ProbeRequest) {
-	result := &ScanResult{}
+// probe probes a specific IP and passes the result to a channel.
+func probe(ch chan<- *ProbeResult, ip net.IP, prober Prober) {
+	result := &ProbeResult{}
 
-	dev, err := Probe(&http.Client{}, ip, prs...)
+	dev, err := Probe(&http.Client{}, ip, prober)
 	if err != nil {
 		result.err = &ProbeError{ip: ip, err: err}
 		ch <- result
@@ -74,16 +64,16 @@ func scanIP(ch chan<- *ScanResult, ip net.IP, prs ...ProbeRequest) {
 // The usable IP addresses of a /24 subnet.
 const subnet24 = 254
 
-// Scan the network for IoT devices using Probe requests.
-func (t *Tuner) Scan(ip net.IP, prs ...ProbeRequest) error {
+// Scan the network with an IoT device prober.
+func (t *Tuner) Scan(ip net.IP, prober Prober) error {
 	// Cleanup before scanning
 	t.devices = Devices{}
 
-	ch := make(chan *ScanResult)
+	ch := make(chan *ProbeResult)
 
 	var octet byte
 	for octet = 1; octet <= subnet24; octet++ {
-		go scanIP(ch, net.IPv4(ip[0], ip[1], ip[2], octet), prs...)
+		go probe(ch, net.IPv4(ip[0], ip[1], ip[2], octet), prober)
 	}
 
 	errs := ProbeErrors{}
