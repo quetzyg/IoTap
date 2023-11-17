3016d4f530d0ed630228fcdda7638c7a60818cee
Quetzy Garcia <quetzyg@altek.org>
2023-11-17 16:25:47 +0000

    feat(device): update the IoTScript type and implement a LoadScript() function

--- device/scripter.go
+++ device/scripter.go
@@ -1,22 +1,35 @@
 package device
 
 import (
-	"io"
 	"net/http"
+	"os"
+	"path"
 )
 
 // IoTScript holds the name and the contents of an IoT device script.
 type IoTScript struct {
 	name string
-	code io.Reader
+	code []byte
 }
 
 func (s *IoTScript) Name() string {
 	return s.name
 }
 
-func (s *IoTScript) Code() io.Reader {
+func (s *IoTScript) Code() []byte {
 	return s.code
+}
+
+func LoadScript(file string) (*IoTScript, error) {
+	content, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return &IoTScript{
+		name: path.Base(file),
+		code: content,
+	}, nil
 }
 
 // Scripter is an interface that provides a standard way to set a script on IoT devices.
