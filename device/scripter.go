822c64fc84fffd550c48840897f9705afcc3bb1f
Quetzy Garcia <quetzyg@altek.org>
2023-11-17 21:33:32 +0000

    feat(device): initial Script procedure implementation

--- device/scripter.go
+++ device/scripter.go
@@ -1,9 +1,12 @@
 package device
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"path"
+
+	iotune "github.com/Stowify/IoTune"
 )
 
 // IoTScript holds the name and the contents of an IoT device script.
@@ -35,4 +38,41 @@ func LoadScript(file string) (*IoTScript, error) {
 // Scripter is an interface that provides a standard way to set a script on IoT devices.
 type Scripter interface {
 	ScriptRequests(*IoTScript) ([]*http.Request, error)
+}
+
+// Script is a procedure implementation designed to upload a script to an IoT device.
+var Script = func(tun *Tuner, dev Resource, ch chan<- *ProcedureResult) {
+	res, ok := dev.(Scripter)
+	if !ok {
+		ch <- &ProcedureResult{
+			err: fmt.Errorf("%w: script", ErrUnsupportedProcedure),
+			dev: dev,
+		}
+		return
+	}
+
+	rs, err := res.ScriptRequests(tun.script)
+	if err != nil {
+		ch <- &ProcedureResult{
+			dev: dev,
+			err: err,
+		}
+		return
+	}
+
+	client := &http.Client{}
+
+	for _, r := range rs {
+		if err = iotune.Dispatch(client, r, nil); err != nil {
+			ch <- &ProcedureResult{
+				dev: dev,
+				err: err,
+			}
+			return
+		}
+	}
+
+	ch <- &ProcedureResult{
+		dev: dev,
+	}
 }
