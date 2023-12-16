1f571c64fecbb264475f56ceea1cfc10e44a2a39
Quetzy Garcia <quetzyg@altek.org>
2023-12-16 17:03:32 +0000

    feat(Shelly Gen2): apply configuration strategy logic

--- shellygen2/config.go
+++ shellygen2/config.go
@@ -1,20 +1,23 @@
 package shellygen2
 
+import "github.com/Stowify/IoTune/device"
+
 // The settings type increases flexibility when dealing with
 // configuration payloads expected by the Shelly Gen2 device API.
 type settings map[string]any
 
 // Config implementation for the Shelly Gen2 driver.
 type Config struct {
-	BLE    *settings    `json:"ble,omitempty"`
-	Cloud  *settings    `json:"cloud,omitempty"`
-	Eth    *settings    `json:"eth,omitempty"`
-	Input  *[]*settings `json:"input,omitempty"`
-	MQTT   *settings    `json:"mqtt,omitempty"`
-	Switch *[]*settings `json:"switch,omitempty"`
-	Sys    *settings    `json:"sys,omitempty"`
-	Wifi   *settings    `json:"wifi,omitempty"`
-	WS     *settings    `json:"ws,omitempty"`
+	Strategy *device.Strategy `json:"strategy,omitempty"`
+	BLE      *settings        `json:"ble,omitempty"`
+	Cloud    *settings        `json:"cloud,omitempty"`
+	Eth      *settings        `json:"eth,omitempty"`
+	Input    *[]*settings     `json:"input,omitempty"`
+	MQTT     *settings        `json:"mqtt,omitempty"`
+	Switch   *[]*settings     `json:"switch,omitempty"`
+	Sys      *settings        `json:"sys,omitempty"`
+	Wifi     *settings        `json:"wifi,omitempty"`
+	WS       *settings        `json:"ws,omitempty"`
 }
 
 // Driver name of this Config implementation.
