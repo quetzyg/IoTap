ed7f873f9cc7ea696e6de4270e1b955cac84ae1c
Quetzy Garcia <quetzyg@altek.org>
2023-12-26 02:00:56 +0000

    tests(devices): add coverage for the Strategy Listed() and Excluded() methods

--- device/strategy_test.go
+++ device/strategy_test.go
@@ -92,3 +92,126 @@ func TestStrategy_UnmarshalJSON(t *testing.T) {
 		})
 	}
 }
+
+type device struct {
+	mac net.HardwareAddr
+}
+
+func (d *device) Driver() string {
+	return "test"
+}
+
+func (d *device) IP() net.IP {
+	return net.IP{}
+}
+
+func (d *device) MAC() net.HardwareAddr {
+	return d.mac
+}
+
+func (d *device) ID() string {
+	return "test"
+}
+
+var mac = net.HardwareAddr{20, 6, 18, 220, 122, 240}
+
+func TestStrategy_Listed(t *testing.T) {
+	tests := []struct {
+		name   string
+		str    *Strategy
+		dev    Resource
+		listed bool
+	}{
+		{
+			name: "device is not listed",
+			str: &Strategy{
+				mode: blacklist,
+				devices: []net.HardwareAddr{
+					mac,
+				},
+			},
+			dev: &device{},
+		},
+		{
+			name: "device is listed",
+			str: &Strategy{
+				mode: whitelist,
+				devices: []net.HardwareAddr{
+					mac,
+				},
+			},
+			dev:    &device{mac: mac},
+			listed: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			listed := test.str.Listed(test.dev)
+			if listed != test.listed {
+				t.Fatalf("expected %t, got %t", test.listed, listed)
+			}
+		})
+	}
+}
+
+func TestStrategy_Excluded(t *testing.T) {
+	tests := []struct {
+		name     string
+		str      *Strategy
+		dev      Resource
+		excluded bool
+	}{
+		{
+			name: "whitelist: device is excluded",
+			str: &Strategy{
+				mode: whitelist,
+				devices: []net.HardwareAddr{
+					mac,
+				},
+			},
+			dev:      &device{},
+			excluded: true,
+		},
+		{
+			name: "whitelist: device is included",
+			str: &Strategy{
+				mode: whitelist,
+				devices: []net.HardwareAddr{
+					mac,
+				},
+			},
+			dev: &device{mac: mac},
+		},
+		{
+			name: "blacklist: device is excluded",
+			str: &Strategy{
+				mode: blacklist,
+				devices: []net.HardwareAddr{
+					mac,
+				},
+			},
+			dev:      &device{mac: mac},
+			excluded: true,
+		},
+		{
+			name: "blacklist: device is included",
+			str: &Strategy{
+				mode: blacklist,
+				devices: []net.HardwareAddr{
+					mac,
+				},
+			},
+			dev: &device{},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			excluded := test.str.Excluded(test.dev)
+			if excluded != test.excluded {
+				t.Fatalf("expected %t, got %t", test.excluded, excluded)
+			}
+		})
+	}
+}
