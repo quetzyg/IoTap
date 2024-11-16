413e5e85feef25e57f71386c92be26a50c887d6d
Quetzy Garcia <quetzyg@altek.org>
2024-11-16 22:01:15 +0000

    feat(flag): add custom string flag type implementation

---
+++ flag.go
@@ -0,0 +1,39 @@
+package iotune
+
+import (
+	"fmt"
+	"strings"
+)
+
+// StrFlag is a custom flag type representing a string,
+// restricted to a predefined set of options.
+type StrFlag struct {
+	options []string
+	value   string
+}
+
+// String implements the Stringer interface.
+func (f *StrFlag) String() string {
+	return f.value
+}
+
+// Set the flag value after validating the input.
+func (f *StrFlag) Set(value string) error {
+	for _, v := range f.options {
+		if value == v {
+			f.value = value
+
+			return nil
+		}
+	}
+
+	return fmt.Errorf("expected one of: %s", strings.Join(f.options, ", "))
+}
+
+// NewStrFlag creates a new StrFlag instance.
+func NewStrFlag(def string, options ...string) *StrFlag {
+	return &StrFlag{
+		options: options,
+		value:   def,
+	}
+}
