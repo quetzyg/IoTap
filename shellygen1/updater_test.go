b7503add782b405fe98a14eb4b8bb49d4464d9bf
Quetzy Garcia <quetzyg@altek.org>
2024-12-09 11:21:27 +0000

    tests(shelly_gen1): add coverage for updater implementation

---
+++ shellygen1/updater_test.go
@@ -0,0 +1,39 @@
+package shellygen1
+
+import (
+	"net"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestDevice_UpdateRequest(t *testing.T) {
+	dev := &Device{
+		ip: net.ParseIP("192.168.146.123"),
+	}
+
+	r, err := dev.UpdateRequest()
+	if err != nil {
+		t.Fatalf("expected nil, got %v", err)
+	}
+
+	if r.Method != http.MethodGet {
+		t.Fatalf("expected %s, got %s", http.MethodGet, r.Method)
+	}
+
+	expectedURL := "http://192.168.146.123/ota?update=true"
+	if r.URL.String() != expectedURL {
+		t.Fatalf("expected %s, got %s", expectedURL, r.URL.String())
+	}
+
+	expectedHeaders := http.Header{
+		"Content-Type": []string{"application/json"},
+	}
+	if !reflect.DeepEqual(expectedHeaders, r.Header) {
+		t.Fatalf("expected %s, got %s", expectedHeaders, r.Header)
+	}
+
+	if r.Body != nil {
+		t.Fatalf("expected nil, got %#v", r.Body)
+	}
+}
