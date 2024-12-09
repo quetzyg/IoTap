3f8db044871017ca164b9fa4b5c1734480615911
Quetzy Garcia <quetzyg@altek.org>
2024-12-09 11:18:10 +0000

    tests(shelly_gen2): add coverage for rebooter implementation

---
+++ shellygen2/rebooter_test.go
@@ -0,0 +1,46 @@
+package shellygen2
+
+import (
+	"io"
+	"net"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestDevice_RebootRequest(t *testing.T) {
+	dev := &Device{
+		ip: net.ParseIP("192.168.146.123"),
+	}
+
+	r, err := dev.RebootRequest()
+	if err != nil {
+		t.Fatalf("expected nil, got %v", err)
+	}
+
+	if r.Method != http.MethodPost {
+		t.Fatalf("expected %s, got %s", http.MethodPost, r.Method)
+	}
+
+	expectedURL := "http://192.168.146.123/rpc"
+	if r.URL.String() != expectedURL {
+		t.Fatalf("expected %s, got %s", expectedURL, r.URL.String())
+	}
+
+	expectedHeaders := http.Header{
+		"Content-Type": []string{"application/json"},
+	}
+	if !reflect.DeepEqual(expectedHeaders, r.Header) {
+		t.Fatalf("expected %s, got %s", expectedHeaders, r.Header)
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		t.Fatalf("expected nil, got %v", err)
+	}
+
+	expectedBody := `{"id":0,"src":"IoTap","method":"Shelly.Reboot"}`
+	if string(body) != expectedBody {
+		t.Fatalf("expected %s, got %s", expectedBody, body)
+	}
+}
