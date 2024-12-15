0ddb9579be14dc300532ff203a87d91e46e06424
Quetzy Garcia <quetzyg@altek.org>
2024-12-15 15:05:00 +0000

    tests(shellygen2): add prober code coverage

---
+++ shellygen2/prober_test.go
@@ -0,0 +1,46 @@
+package shellygen2
+
+import (
+	"errors"
+	"net"
+	"reflect"
+	"testing"
+)
+
+func TestProber_Request(t *testing.T) {
+	tests := []struct {
+		name string
+		ip   net.IP
+		dev  *Device
+		uri  string
+		err  error
+	}{
+		{
+			name: "success",
+			ip:   net.ParseIP("192.168.146.123"),
+			dev: &Device{
+				ip: net.ParseIP("192.168.146.123"),
+			},
+			uri: "http://192.168.146.123/shelly",
+			err: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			r, dev, err := (&Prober{}).Request(test.ip)
+
+			if r.URL.String() != test.uri {
+				t.Fatalf("expected %#v, got %#v", test.uri, r.URL.String())
+			}
+
+			if !reflect.DeepEqual(dev, test.dev) {
+				t.Fatalf("expected %#v, got %#v", test.dev, dev)
+			}
+
+			if !errors.Is(err, test.err) {
+				t.Fatalf("expected %#v, got %#v", test.err, err)
+			}
+		})
+	}
+}
