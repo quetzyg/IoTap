f4a62e744ff9d04117de582f1ebe3dd6200e8f7f
Quetzy Garcia <quetzyg@altek.org>
2024-12-15 11:18:55 +0000

    tests(errors): add code coverage

---
+++ device/errors_test.go
@@ -0,0 +1,32 @@
+package device
+
+import (
+	"net"
+	"testing"
+)
+
+func TestProbeError_Error(t *testing.T) {
+	const expected = "192.168.146.123: use of closed network connection\n"
+
+	err := &ProbeError{
+		ip:  net.ParseIP("192.168.146.123"),
+		err: net.ErrClosed,
+	}
+
+	if err.Error() != expected {
+		t.Fatalf("expected %q, got %q", expected, err.Error())
+	}
+}
+
+func TestErrors_Error(t *testing.T) {
+	const expected = "unexpected IoT device\ndevice driver mismatch"
+
+	err := &Errors{
+		ErrUnexpected,
+		ErrDriverMismatch,
+	}
+
+	if err.Error() != expected {
+		t.Fatalf("expected %q, got %q", expected, err.Error())
+	}
+}
