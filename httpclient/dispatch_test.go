73a8773e3abbb5607d90cbc9ff9f3c389eb1bcac
Quetzy Garcia <quetzyg@altek.org>
2025-01-09 14:02:01 +0000

    tests(httpclient): increase coverage for the Dispatch() function

--- httpclient/dispatch_test.go
+++ httpclient/dispatch_test.go
@@ -34,6 +34,23 @@ func (badReader) Close() error {
 	return nil
 }
 
+type challenger struct {
+	req *http.Request
+	err error
+}
+
+func (c *challenger) ChallengeAccepted(resp *http.Response) bool {
+	return resp.StatusCode == http.StatusUnauthorized
+}
+
+func (c *challenger) ChallengeResponse(_ *http.Request, _ *http.Response) (*http.Request, error) {
+	if c.err != nil {
+		return nil, c.err
+	}
+
+	return c.req, nil
+}
+
 var (
 	uri = &url.URL{
 		Scheme: "http",
@@ -54,6 +71,14 @@ func TestDispatch(t *testing.T) {
 		err          error
 	}{
 		{
+			name: "failure: clone request error",
+			req: &http.Request{
+				Body: &badReader{},
+			},
+			challenger: &challenger{},
+			err:        io.ErrUnexpectedEOF,
+		},
+		{
 			name: "failure: timeout",
 			req: &http.Request{
 				Method: http.MethodGet,
@@ -64,6 +89,44 @@ func TestDispatch(t *testing.T) {
 				err:      context.DeadlineExceeded,
 			},
 			err: context.DeadlineExceeded,
+		},
+		{
+			name: "failure: challenge accepted error",
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL:    uri,
+			},
+			roundTripper: &RoundTripper{
+				response: &http.Response{
+					StatusCode: http.StatusUnauthorized,
+				},
+			},
+			challenger: &challenger{
+				err: http.ErrHeaderTooLong,
+			},
+			err: http.ErrHeaderTooLong,
+		},
+		{
+			name: "failure: challenge unauthorised",
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL:    uri,
+			},
+			roundTripper: &RoundTripper{
+				response: &http.Response{
+					StatusCode: http.StatusUnauthorized,
+				},
+			},
+			challenger: &challenger{
+				req: &http.Request{
+					Method: http.MethodGet,
+					URL:    uri,
+					Header: http.Header{
+						AuthorizationHeader: []string{`Digest foobar`},
+					},
+				},
+			},
+			err: errRequestUnauthorised,
 		},
 		{
 			name: "failure: unable to read body",
@@ -80,6 +143,19 @@ func TestDispatch(t *testing.T) {
 			err: io.ErrUnexpectedEOF,
 		},
 		{
+			name: "success: unmarshal body",
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL:    uri,
+			},
+			roundTripper: &RoundTripper{
+				response: &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(strings.NewReader("{}")),
+				},
+			},
+			v: &v,
+		}, {
 			name: "failure: bad request",
 			req: &http.Request{
 				Method: http.MethodGet,
@@ -106,20 +182,6 @@ func TestDispatch(t *testing.T) {
 				},
 			},
 		},
-		{
-			name: "success: unmarshal body",
-			req: &http.Request{
-				Method: http.MethodGet,
-				URL:    uri,
-			},
-			roundTripper: &RoundTripper{
-				response: &http.Response{
-					StatusCode: http.StatusOK,
-					Body:       io.NopCloser(strings.NewReader("{}")),
-				},
-			},
-			v: &v,
-		},
 	}
 
 	for _, test := range tests {
@@ -129,9 +191,22 @@ func TestDispatch(t *testing.T) {
 			}
 
 			err := Dispatch(client, test.req, test.challenger, test.v)
-			if !errors.Is(err, test.err) {
-				t.Fatalf("expected %#v, got %#v", test.err, err)
+
+			var urlError *url.Error
+			switch {
+			case errors.As(test.err, &urlError):
+				var ue *url.Error
+				if errors.As(err, &ue) {
+					return
+				}
+
+			default:
+				if errors.Is(err, test.err) {
+					return
+				}
 			}
+
+			t.Fatalf("expected %#v, got %#v", test.err, err)
 		})
 	}
 }
