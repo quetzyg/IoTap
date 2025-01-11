f29a8b7d2d72a22bbf7eec8f9a32457f3cb80f3d
Quetzy Garcia <quetzyg@altek.org>
2025-01-11 14:18:02 +0000

    feat(device): implement GetProbers() function and add test coverage

--- device/prober.go
+++ device/prober.go
@@ -21,3 +21,22 @@ var proberRegistry = make(map[string]ProberProvider)
 func RegisterProber(driver string, prov ProberProvider) {
 	proberRegistry[driver] = prov
 }
+
+// GetProbers returns a slice of Prober implementations based on the specified driver.
+// If the driver is "all", it returns a slice containing all registered probers.
+// If a specific driver name is provided, it returns a slice with the prober for that driver.
+func GetProbers(driver string) []Prober {
+	if driver != AllDrivers {
+		return []Prober{
+			proberRegistry[driver](),
+		}
+	}
+
+	var probers []Prober
+
+	for _, prov := range proberRegistry {
+		probers = append(probers, prov())
+	}
+
+	return probers
+}
--- device/prober_test.go
+++ device/prober_test.go
@@ -1,6 +1,9 @@
 package device
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestRegisterProber(t *testing.T) {
 	if len(proberRegistry) != 0 {
@@ -13,5 +16,52 @@ func TestRegisterProber(t *testing.T) {
 
 	if len(proberRegistry) != 1 {
 		t.Fatalf("Prober registry should have one registered prober, %d found", len(proberRegistry))
+	}
+}
+
+func TestGetProbers(t *testing.T) {
+	tests := []struct {
+		name    string
+		driver  string
+		probers []Prober
+	}{
+		{
+			name:   "return all probers",
+			driver: AllDrivers,
+			probers: []Prober{
+				&prober{},
+				&prober{},
+				&prober{},
+			},
+		},
+		{
+			name:   "return single prober",
+			driver: "bar",
+			probers: []Prober{
+				&prober{},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			proberRegistry = map[string]ProberProvider{
+				"foo": func() Prober {
+					return &prober{}
+				},
+				"bar": func() Prober {
+					return &prober{}
+				},
+				"baz": func() Prober {
+					return &prober{}
+				},
+			}
+
+			probers := GetProbers(test.driver)
+
+			if !reflect.DeepEqual(probers, test.probers) {
+				t.Fatalf("expected %#v, got %#v", test.probers, probers)
+			}
+		})
 	}
 }
