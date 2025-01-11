c13b242481cba2df93b06b3a47e7cfd2c84696a9
Quetzy Garcia <quetzyg@altek.org>
2025-01-11 14:19:55 +0000

    feat(shellygen2): register Prober

--- shellygen2/init.go
+++ shellygen2/init.go
@@ -5,6 +5,10 @@ import "github.com/quetzyg/IoTap/device"
 // init registers this package's device implementations.
 // It will be automatically called when the package is imported.
 func init() {
+	device.RegisterProber(Driver, func() device.Prober {
+		return &Prober{}
+	})
+
 	device.RegisterConfig(Driver, func() device.Config {
 		return &Config{}
 	})
