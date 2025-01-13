1c4fa01a5d56c865ed74173ef95907a9c8bae72f
Quetzy Garcia <quetzyg@altek.org>
2025-01-13 22:30:46 +0000

    tests(shellygen1): define the expected *http.Request instead of using the request() function

--- shellygen1/configurer_test.go
+++ shellygen1/configurer_test.go
@@ -2,12 +2,14 @@ package shellygen1
 
 import (
 	"errors"
+	"net"
 	"net/http"
 	"net/url"
 	"reflect"
 	"testing"
 
 	"github.com/quetzyg/IoTap/device"
+	"github.com/quetzyg/IoTap/httpclient"
 )
 
 // config implementation for testing purposes.
@@ -41,20 +43,17 @@ func compareHTTPRequests(req1, req2 *http.Request) bool {
 func TestDevice_ConfigureRequests(t *testing.T) {
 	tests := []struct {
 		name string
-		dev  *Device
 		cfg  device.Config
 		rs   []*http.Request
 		err  error
 	}{
 		{
 			name: "failure: driver mismatch",
-			dev:  &Device{},
 			cfg:  &config{},
 			err:  device.ErrDriverMismatch,
 		},
 		{
 			name: "failure: policy exclusion",
-			dev:  &Device{},
 			cfg: &Config{
 				Policy: &device.Policy{
 					Mode: device.PolicyModeWhitelist,
@@ -64,25 +63,42 @@ func TestDevice_ConfigureRequests(t *testing.T) {
 		},
 		{
 			name: "success: single settings",
-			dev:  &Device{},
 			cfg: &Config{
 				Settings: &settings{
 					"discoverable": true,
 				},
 			},
 			rs: func() []*http.Request {
-				r1, _ := request(&Device{}, "settings", url.Values{
-					"discoverable": []string{"true"},
-				})
+				r1 := &http.Request{
+					Method: http.MethodGet,
+					URL: &url.URL{
+						Scheme:   "http",
+						Host:     "192.168.146.123",
+						Path:     "settings",
+						RawQuery: "discoverable=true",
+					},
+					Header: http.Header{},
+				}
 
-				r2, _ := request(&Device{}, rebootPath, nil)
+				r1.Header.Set(httpclient.ContentTypeHeader, httpclient.JSONMimeType)
+
+				r2 := &http.Request{
+					Method: http.MethodGet,
+					URL: &url.URL{
+						Scheme: "http",
+						Host:   "192.168.146.123",
+						Path:   rebootPath,
+					},
+					Header: http.Header{},
+				}
+
+				r2.Header.Set(httpclient.ContentTypeHeader, httpclient.JSONMimeType)
 
 				return []*http.Request{r1, r2}
 			}(),
 		},
 		{
 			name: "success: settings slice",
-			dev:  &Device{},
 			cfg: &Config{
 				SettingsRelay: &[]*settings{
 					{
@@ -97,26 +113,41 @@ func TestDevice_ConfigureRequests(t *testing.T) {
 				},
 			},
 			rs: func() []*http.Request {
-				r1, _ := request(&Device{}, "settings/relay/0", url.Values{
-					"name":           []string{"null"},
-					"appliance_type": []string{"lock"},
-					"default_state":  []string{"off"},
-					"btn_type":       []string{"detached"},
-					"btn_reverse":    []string{"true"},
-					"auto_on":        []string{"0"},
-					"auto_off":       []string{"3"},
-				})
+				r1 := &http.Request{
+					Method: http.MethodGet,
+					URL: &url.URL{
+						Scheme:   "http",
+						Host:     "192.168.146.123",
+						Path:     "settings/relay/0",
+						RawQuery: "appliance_type=lock&auto_off=3&auto_on=0&btn_reverse=true&btn_type=detached&default_state=off&name=null",
+					},
+					Header: http.Header{},
+				}
 
-				r2, _ := request(&Device{}, "reboot", nil)
+				r1.Header.Set(httpclient.ContentTypeHeader, httpclient.JSONMimeType)
+
+				r2 := &http.Request{
+					Method: http.MethodGet,
+					URL: &url.URL{
+						Scheme: "http",
+						Host:   "192.168.146.123",
+						Path:   rebootPath,
+					},
+					Header: http.Header{},
+				}
+
+				r2.Header.Set(httpclient.ContentTypeHeader, httpclient.JSONMimeType)
 
 				return []*http.Request{r1, r2}
 			}(),
 		},
 	}
 
+	shelly1 := &Device{ip: net.ParseIP("192.168.146.123")}
+
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			rs, err := test.dev.ConfigureRequests(test.cfg)
+			rs, err := shelly1.ConfigureRequests(test.cfg)
 
 			for i, r := range rs {
 				if !compareHTTPRequests(r, test.rs[i]) {
